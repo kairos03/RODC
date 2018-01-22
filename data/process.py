0f9535abe5c4cb10a46efb9c8b73679f018d8a5e
karios03 <kairos9603@gmail.com>
2018-01-22 16:26:52 +0100

    fix wrong spell

--- data/process.py
+++ data/process.py
@@ -13,8 +13,6 @@ DATA_PATH = 'data/'
 PEPTIDE_PATH = DATA_PATH+'peptide/'
 PARTICLE_PATH = DATA_PATH+'particle/'
 IMAGE_TRAIN_DATASET_PATH = DATA_PATH + 'image_train.h5'
-IMAGE_PATH = DATA_PATH + 'image.pkl'
-LABLE_PATH = DATA_PATH + 'lable.pkl'
 
 def make_image_train_dataset(path=IMAGE_TRAIN_DATASET_PATH):
     imgs = []
@@ -32,30 +30,30 @@ def make_image_train_dataset(path=IMAGE_TRAIN_DATASET_PATH):
     imgs = np.asarray(imgs)
     assert imgs.shape[0] == 900
 
-    # make lables
+    # make labels
     zero = np.zeros((450, 1))
     one = np.ones((450, 1))
-    peptide_lables = np.concatenate([one, zero], axis=1)
-    particle_lables = np.concatenate([zero, one], axis=1)
-    lables = np.concatenate([peptide_lables, particle_lables])
-    assert lables.shape == (900, 2)
-    assert (lables[449] == np.array([1, 0])).all() 
-    assert (lables[450] == np.array([0, 1])).all() 
+    peptide_labels = np.concatenate([one, zero], axis=1)
+    particle_labels = np.concatenate([zero, one], axis=1)
+    labels = np.concatenate([peptide_labels, particle_labels])
+    assert labels.shape == (900, 2)
+    assert (labels[449] == np.array([1, 0])).all() 
+    assert (labels[450] == np.array([0, 1])).all() 
 
     # save
     with h5py.File(path, 'w') as hf:
         hf.create_dataset("images", data=imgs, compression="gzip", compression_opts=5)
-        hf.create_dataset("lables", data=lables, compression="gzip", compression_opts=5)
+        hf.create_dataset("labels", data=labels, compression="gzip", compression_opts=5)
     print("SAVED", path)
 
 
 def load_image_train_dataset(path=IMAGE_TRAIN_DATASET_PATH):
     with h5py.File(path, 'r') as hf:
         images = hf.get("images")[:]
-        lables = hf.get("lables")[:]
-    print("LOADED", path, images.shape, lables.shape)
+        labels = hf.get("labels")[:]
+    print("LOADED", path, images.shape, labels.shape)
 
-    return images, lables
+    return images, labels
 
 
 if __name__ == '__main__':
