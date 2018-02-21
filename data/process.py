264569259ba77da81deedef580848891c1aae9f2
moonhyeok song <firefinger07@gmail.com>
2018-02-21 18:27:35 +0100

    complete make classification dataset

--- data/process.py
+++ data/process.py
@@ -14,12 +14,14 @@ import pandas as pd
 import h5py
 import cv2
 
-from data.hb_process import get_roi
-from data.hb_process import get_nofcomp
-from data.hb_process import cvtColor
+
+# from data.hb_process import get_roi
+# from data.hb_process import get_nofcomp
+# from data.hb_process import cvtColor
 
 
 DATA_PATH = 'data/seg_data/'
+DATA_PATH2 = 'data/realimages/'
 
 PARTICLE = [1.,0.,0.]
 PEPTIDE = [0.,1.,0.]
@@ -40,6 +42,7 @@ ORIGIN_PATH = DATA_PATH + 'images/'
 MASK_PATH = DATA_PATH + 'labels/'
 ANNO_PATH = DATA_PATH + 'annos/'
 FCN_TRAIN_DATASET_PATH = DATA_PATH + 'fcn_train.h5'
+CLASSFICATION_TRAIN_PATH = DATA_PATH2 + 'classification_train.h5'
 
 
 def make_image_train_dataset(path=IMAGE_TRAIN_DATASET_PATH):
@@ -74,9 +77,8 @@ def load_image_train_dataset(path=IMAGE_TRAIN_DATASET_PATH):
     df = pd.read_hdf(path)
     print("LOADED", path, df.shape)
     return df
-
-
 def make_detacion_train_dataset(path=DETACTION_OBJECTS_PATH):
+    
     """
     """
     annotaion_path = 'Annotation/'
@@ -113,15 +115,12 @@ def make_detacion_train_dataset(path=DETACTION_OBJECTS_PATH):
     dataset.to_hdf(DETACTION_TRAIN_DATASET_PATH, 'detacion')
     # print(dataset.head)
     print("SAVED", DETACTION_TRAIN_DATASET_PATH, dataset.shape)
-
-
 def load_detacion_train_dataset(path=DETACTION_TRAIN_DATASET_PATH):
     """
     """
     df = pd.read_hdf(path)
     print("LOADED", path, df.shape)
     return df
-
 
 def image_to_anno(img, width=256, height=256):
 
@@ -134,11 +133,11 @@ def image_to_anno(img, width=256, height=256):
 
     return mask
 
-
 def make_fcn_train_dataset(path=FCN_TRAIN_DATASET_PATH):
     df = pd.DataFrame()
     filenames = np.array(os.listdir(ORIGIN_PATH))
     df['filename'] = filenames
+
 
     # label to anno_img
     for name in filenames:
@@ -151,7 +150,6 @@ def make_fcn_train_dataset(path=FCN_TRAIN_DATASET_PATH):
     df.to_hdf(path, 'detacion')
     print("SAVED", path, df.shape)
 
-
 def load_fcn_train_dataset(path=FCN_TRAIN_DATASET_PATH):
     """
     """
@@ -159,6 +157,73 @@ def load_fcn_train_dataset(path=FCN_TRAIN_DATASET_PATH):
     print("LOADED", path, df.shape)
     return df
 
+def make_classification_dataset(path=CLASSFICATION_TRAIN_PATH):
+    df = pd.DataFrame(columns=['filenames', 'is_contacted'])
+
+    c_images, c_filenames = concatenated_image(DATA_PATH2 + 'contact/')
+    s_images, s_filenames = concatenated_image(DATA_PATH2 + 'separate/')
+    n_c_img = c_images.shape[0]//2
+    n_s_img = s_images.shape[0]//2
+
+    # contact + separate image
+    # images = np.concatenate((c_images, s_images))
+    filenames = np.concatenate((c_filenames, s_filenames))
+    # contact + separate labels
+    labels = np.concatenate((np.ones(n_c_img, ), np.zeros(n_s_img,)))
+
+    print(filenames, filenames.shape, labels, labels.shape)
+    df['filenames'] = filenames
+    df['is_contacted'] = labels
+
+    # save
+    df.to_hdf(path, 'classification')
+    print("SAVED", path, df.shape)
+
+def concatenated_image(path):
+    """contact front and right data""" 
+    # read images
+    f_fnames, f_images = read_image(path+'front/')
+    r_fnames, r_images = read_image(path+'right/')
+
+    #for r in range(r_fnames.shape[0]):
+    #    for f in range(f_fnames.shape[0]):
+    #        if r_fnames[r] == f_fnames[f]:
+    #            break
+    #        elif f == (f_fnames.shape[0]-1):
+    #            print(r_fnames[r])
+    #            print(r_fnames.shape)
+                
+    # concat two images
+    images = np.concatenate((f_images, r_images), 0) # f,f,f,...,r,r,,,,
+
+    return images, f_fnames
+
+def read_image(path):
+    """ Read images from path and return fnames, images"""
+    print(os.listdir('.'))
+    print(path)
+    fnames = np.array(os.listdir(path))
+    fnames.sort()
+    # resize_path = path.replace(DATA_PATH2, DATA_PATH2 + 'resize/')
+
+    imgs = []
+    for name in fnames:
+        img = misc.imread(path+name)
+        # print(img.shape, path+name)
+        img = misc.imresize(img, (256, 256))
+        imgs.append(img)
+    #    misc.imsave(resize_path+name, img)
+    print(img, img.shape)
+    imgs = np.stack(imgs)
+    print(img, img.shape)
+    return fnames, imgs
+
+def load_classification_dataset(path=CLASSFICATION_TRAIN_PATH):
+    """
+    """
+    df = pd.read_hdf(path)
+    print("LOADED", path, df.shape)
+    return df
 
 def pre_process(image_names, path, size=256, interp='bilinear'):
     imgs = []
@@ -209,5 +274,9 @@ if __name__ == '__main__':
     # make_detacion_train_dataset()
     # load_detacion_train_dataset()
 
-    make_fcn_train_dataset()
-    load_fcn_train_dataset()
+    # before deconvenet_train
+    # make_fcn_train_dataset()
+    # load_fcn_train_dataset()
+
+    make_classification_dataset()
+    load_classification_dataset()
